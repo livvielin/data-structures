c2992cf3ef4baa9218b2aa605a9c5ab9c1b5c9b3
Livvie Lin <livvie.lin@gmail.com>
2015-08-20 08:57:18 -0700

    Started linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,25 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (list.head === null) {
+      list.head = Node(value);
+      list.tail = Node(value);
+    } else {
+      this.prev = list.tail;
+      this.prev.next = Node(value);
+      list.tail = Node(value);
+    }
   };
 
   list.removeHead = function(){
+    var temp = list.head;
+    if(list.head === list.tail) {
+      list.head = null;
+    } else {
+      list.head = list.head.next;
+      this.prev = null;
+    }
+    return temp;
   };
 
   list.contains = function(target){
@@ -20,6 +36,7 @@ var Node = function(value){
 
   node.value = value;
   node.next = null;
+  node.prev = null;
 
   return node;
 };
