8c8c98d23740f09e457f986a0e442d4edf15b737
livvielin <livvie.lin@gmail.com>
2015-08-20 11:41:14 -0700

    Defines Tree to pass most tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,25 +2,33 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
 
-
-  // your code here
-  newTree.children = null;  // fix me
-
+// newTree.children = {};
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var tree = Tree(value);
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target){
 
+  var result = false;
+  if(this.value === target) {
+    result = true;
+  } else if(this.children.length !== 0) {
+    for(var i = 0; i < this.children.length; i++) {
+      return this.children[i].contains(target);  
+    }
+  } 
+  return result;
 };
 
 
