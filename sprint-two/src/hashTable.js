e7b4b97222147a31ce00f3bae03593be31199b75
Livvie Lin <livvie.lin@gmail.com>
2015-08-21 11:14:21 -0700

    Defines HashTable to pass all tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,20 +5,41 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, v);
+  if (this._storage.get(i) === undefined) {
+    this._storage.set(i, []);
+  } else {
+    for(var tuple = 0; tuple < this._storage.get(i).length; tuple++) {
+      if(this._storage.get(i)[tuple][0] === k) {
+        this._storage.get(i)[tuple][1] = v;
+        return;
+      }
+    }
+  }
+  this._storage.get(i).push(entry(k, v));
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  for(var tuple = 0; tuple < this._storage.get(i).length; tuple++) {
+    if(this._storage.get(i)[tuple][0] === k) {
+      return this._storage.get(i)[tuple][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, null);
+  for(var tuple = 0; tuple < this._storage.get(i).length; tuple++) {
+    if(this._storage.get(i)[tuple][0] === k) {
+      this._storage.get(i)[tuple][1] = null;
+    }
+  }
 };
 
-
+var entry = function(key, value) {
+  var prop = [key, value];
+  return prop;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
