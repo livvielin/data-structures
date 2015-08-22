195a604963f7b1ad6f54cfe259174bf7301bb007
Livvie Lin <livvie.lin@gmail.com>
2015-08-21 21:19:22 -0700

    Partially implements hashTable doubling

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,7 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.numKeys = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
@@ -16,6 +17,24 @@ HashTable.prototype.insert = function(k, v){
     }
   }
   this._storage.get(i).push(entry(k, v));
+  this.numKeys++;
+  if(this.numKeys / this._limit >= .75) {
+    this._limit = this._limit * 2;
+    this.reassign();
+  }
+};
+
+HashTable.prototype.reassign = function() {
+  var newHash = this.call();
+  //var newStorage = LimitedArray.call(this._limit);
+  this._storage.each(function(bucket) {
+    for(var tuple = 0; tuple < this._storage.get(i).length; tuple++) {
+      var k = bucket[tuple][0];
+      var v = bucket[tuple][1];
+      newHash.insert(k, v);
+    }
+  });
+  this._storage = newHash._storage;
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -44,3 +63,16 @@ var entry = function(key, value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// var test = new HashTable();
+// test.insert('Steven', 'Larkin');
+// test.insert('Bob', 'Jones');
+// test.insert('Alice', 'Larkin');
+// test.insert('Peter', 'Larkin');
+// test.insert('Na', 'Larkin');
+// test.insert('Carl', 'Larkin');
+// test.insert('Hello', 'World');
+// test.insert('Tom', 'Larkin');
+// test.insert('Tim', 'Larkin');
+// test.insert('Kyle', 'Larkin');
+// test.insert('Hi', 'World');
